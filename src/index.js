ad48ca04530e953ed6189b5fc2ab1d3f3daa4247
Claire Baissac <claire.baissac@yoco.com>
2022-04-05 10:44:28 +0200

    complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import './assets/styles/style.css';
+import Icon from './assets/images/webpack-logo.svg';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick= printMe;
+
+  element.appendChild(btn);
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack demo",
+    }),
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
